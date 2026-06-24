640aeea95485d544a6ed068e94adc90b80a7e471
wh1zzRD <58628211+wh1zzRD@users.noreply.github.com>
2026-06-24 20:01:15 +0200

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+
+        if (text1.charAt(0) == text2.charAt(0)) dp[0][0] = 1;
+        // for (int i = 1; i < text1.length(); i++) {
+        //     if (text1.charAt(i) == text2.charAt(0)) dp[i][0] = 1;
+        //     else dp[i][0] = dp[i-1][0];
+        // }
+        for (int i = 1; i < text2.length(); i++) {
+            if (text2.charAt(i) == text1.charAt(0)) dp[0][i] = 1;
+            else dp[0][i] = dp[0][i-1];
+        }
+
+        for (int i = 1; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                if (j == 0) {
+                    dp[i][j] = Math.max(dp[i-1][j], text1.charAt(i) == text2.charAt(j)?1:0);
+                    continue;
+                }
+                if (text1.charAt(i) == text2.charAt(j)) dp[i][j] = Math.max(dp[i-1][j-1] + 1, Math.max(dp[i-1][j], dp[i][j-1]));
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[text1.length() - 1][text2.length() - 1];
+    }
+}
