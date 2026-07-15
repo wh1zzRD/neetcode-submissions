16695c87e8925a02c17a5d3c9aa1b6a7865fa751
wh1zzRD <58628211+wh1zzRD@users.noreply.github.com>
2026-07-15 20:01:47 +0200

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        int[][] dp = new int[nums.length][2 * sum + 1];
+
+        if (Math.abs(target) > Math.abs(sum)) return 0;
+
+        dp[0][sum + nums[0]] += 1;
+        dp[0][sum - nums[0]] += 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < 2 * sum + 1; j++) {
+                if (j + nums[i + 1] < 2 * sum + 1)
+                    dp[i+1][j + nums[i + 1]] += dp[i][j];
+                if (j - nums[i + 1] >= 0)
+                    dp[i+1][j - nums[i + 1]] += dp[i][j];
+            }
+        }
+
+        return dp[nums.length - 1][sum + target];
+
+        
+    }
+
+    public int nToCoord(int sum, int n) {
+        return sum + n;
+    }
+}
